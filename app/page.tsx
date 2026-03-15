97110fb02213364f55af8b639d0fdab700b2f6d4
neoproxy.art <neoproxy.art@gmail.com>
2026-03-15 13:45:59 +0000

    feat: Set new home page and recover old pages

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ color: 'red' }}>hola mundo</h1>
+    </main>
+  );
+}
